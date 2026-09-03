db0717257bbbda97c3f9a17aa169ab1df15844ba
l1htar.w.ubuntu <ansafbiz@gmail.com>
2026-09-03 17:52:33 +0300

    создал класс конфиг для подключения к mysql базе и отредачил

--- functions.php
+++ functions.php
@@ -14,4 +14,3 @@ function urlIs($value) {
 }
 
 //dd($_SERVER);
-
--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
 
 require "functions.php";
-require "router.php";
+// require "router.php";
+
+require 'Database.php';
 
 
+$config = require 'config.php';
+
+$db = new Database($config['database'], 'root', 'mysqlroot');
+
+//$posts = $db->query("select * from posts where id = 13")->fetchAll(PDO::FETCH_ASSOC);
+$post = $db->query("select * from posts where id = 13")->fetch(PDO::FETCH_ASSOC);
+
+dd($post);
+
+//foreach ($posts as $post) {
+//    echo "<li>" . $post['title'] . "</li>";
+//}
